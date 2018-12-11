859771e153b906c95ba60d9be663df1e9c049ab1
leo <wanglingsong@gmail.com>
2018-12-11 15:40:48 +0800

    Adjust UX

--- pages/collect_task/index.js
+++ pages/collect_task/index.js
@@ -16,12 +16,12 @@ Page({
         let staticImg = this.data.staticImg;
         staticImg[index].photoSrc = res.tempFilePaths[0];
 
-        if(res.tempFiles[0].size <= 10000000){
+        if (res.tempFiles[0].size <= 10000000) {
           //console.log(res.tempFiles[0].size)
           this.setData({
             staticImg: staticImg
           })
-        }else{
+        } else {
           wx.showToast({
             title: "图片不能超过10M"
           })
@@ -48,7 +48,7 @@ Page({
       })
     } else {
       wx.showLoading({
-        title: "上传中...",
+        title: "上传中",
         mask: true,
       })
       this.uploadImg()
@@ -56,13 +56,18 @@ Page({
   },
 
   cancelWork() {
-    if (this.id) {
-      wx.showLoading({
-        title: "加载中...",
-        mask: true,
-      })
-      beevalley.cancelWork(this.token, [this.id], (res) => this.nextWork())
-    }
+    wx.showModal({
+      title: '放弃任务',
+      content: '确定放弃当前任务',
+      confirmText: "知道了",
+      success: function (res) {
+        if (res.confirm) {
+          wx.navigateBack({
+            delta: 1
+          })
+        }
+      }
+    })
   },
 
   uploadImg() {
@@ -72,16 +77,19 @@ Page({
       let uploadFileIds = this.data.staticImg.map(e => e.fileId).filter(f => f)
 
       beevalley.submitWork(this.token, this.id, uploadFileIds, (res) => {
-        that.handleError(res)
-        wx.hideLoading()
-        if (res.statusCode === 200) {
-          wx.showToast({
-            title: "上传成功",
-            success: that.nextWork
-          })
-        } else {
-          wx.showToast({
-            title: "上传失败，请重试"
+        if (that.handleError(res)) {
+          wx.hideLoading()
+          wx.showModal({
+            title: '成功',
+            content: '上传成功',
+            showCancel: false,
+            confirmText: "知道了",
+            success: function () {
+              that.id = null
+              wx.navigateBack({
+                delta: 1
+              })
+            }
           })
         }
       })
@@ -95,18 +103,10 @@ Page({
         that.uploadImg();
       } else {
         beevalley.workFile(this.token, this.id, ele.photoSrc, (res) => {
-          that.handleError(res)
-          if (res.statusCode === 200) {
+          if (that.handleError(res)) {
             ele.fileId = JSON.parse(res.data)[0];  //根据对应的下标，赋值id
             that.countIndex++;
             that.uploadImg();
-          } else {
-            // that.uploadedImages = [];
-            that.countIndex = 0;
-            wx.hideLoading()
-            wx.showToast({
-              title: "上传失败，请重试"
-            })
           }
         })
       }
@@ -116,8 +116,8 @@ Page({
 
   handleError: function (res) {
     if (res.statusCode === 403) {
-      // TODO handle error
-      if (typeof res.data === 'object' && res.data.code === '20') {
+      wx.hideLoading()
+      if (typeof res.data === 'object' && res.data.error && res.data.error.code === '20') {
         wx.showModal({
           title: '任务配额已用完',
           content: '请稍后重试',
@@ -129,6 +129,7 @@ Page({
             })
           }
         })
+        return false
       } else {
         wx.showModal({
           title: '任务超时',
@@ -141,64 +142,95 @@ Page({
             })
           }
         })
+        return false
       }
+    } else if (res.statusCode !== 200) {
+      wx.hideLoading()
+      wx.showModal({
+        title: '系统错误',
+        content: '请稍后重试',
+        showCancel: false,
+        confirmText: "知道了",
+        success: function () {
+          wx.navigateBack({
+            delta: 1
+          })
+        }
+      })
+      return false
     }
+    return true
   },
 
   nextWork() {
-    wx.showLoading({
-      title: "加载中...",
-      mask: true,
-    })
     this.id = null
     // this.uploadedImages = [];
     this.countIndex = 0;
     let that = this
+    wx.showLoading({
+      title: "加载中",
+      mask: true,
+    })
     beevalley.fetchWorks(this.token, "collect", 1, this.packageId, (res) => {
-      that.handleError(res)
-      wx.hideLoading()
-      if (res.data.length > 0) {
-        let work = res.data[0]
-        that.id = work.id;
-        let imgArr = work.meta.samples.map((item) => {
-          return {
-            src: item,
-            editable: true,
-            shouldUpload: true
-          };
-        })
-        if (work.previousWork) {
-          let successImg = this.getDiff(work.previousWork.result, work.meta.rejectedReason);
+      if (that.handleError(res)) {
 
-          successImg.forEach(f => {
-            let index = work.previousWork.result.indexOf(f)
-            imgArr[index].editable = false
-            imgArr[index].shouldUpload = false
-            imgArr[index].fileId = f
+        wx.hideLoading()
+        if (res.data.length > 0) {
 
-            beevalley.downloadWorkFiles(this.token, work.id, f, (res) => {
-              that.handleError(res);
-              // let imageSrc = 'data:image/jpeg;base64,' + wx.arrayBufferToBase64(res.data);
-              let newImgArr = that.data.staticImg
-              newImgArr[index].photoSrc = res.tempFilePath
-              // this.uploadedImages[index] = f;
-              that.setData({//由于是异步所以每次都需要更新数据，
-                staticImg: newImgArr
+          let work = res.data[0]
+          that.id = work.id;
+          let imgArr = work.meta.samples.map((item) => {
+            return {
+              src: item,
+              editable: true,
+              shouldUpload: true
+            };
+          })
+
+          if (work.previousWork) {
+            let successImg = this.getDiff(work.previousWork.result, work.meta.rejectedReason);
+
+            successImg.forEach(f => {
+              let index = work.previousWork.result.indexOf(f)
+              imgArr[index].editable = false
+              imgArr[index].shouldUpload = false
+              imgArr[index].fileId = f
+
+              beevalley.downloadWorkFiles(this.token, work.id, f, (res) => {
+                if (that.handleError(res)) {
+                  let newImgArr = that.data.staticImg
+                  newImgArr[index].photoSrc = res.tempFilePath
+                  that.setData({//由于是异步所以每次都需要更新数据，
+                    staticImg: newImgArr
+                  })
+                }
               })
             })
+          }
+
+          this.setData({
+            staticImg: imgArr,
+            textMessage: work.details
           })
+
+        } else {
+
+          wx.showModal({
+            title: '抱歉',
+            content: '暂时没有任务了',
+            confirmText: "知道了",
+            showCancel: false,
+            success: function () {
+              wx.navigateBack({
+                delta: 1
+              })
+            }
+          })
+
         }
 
-        this.setData({
-          staticImg: imgArr,
-          textMessage: work.details
-        })
-
-      } else {
-        wx.showToast({
-          title: "没有任务了"
-        })
       }
+
     })
   },
 
